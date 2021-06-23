147ca7bac2702a984d4b2bf4ede7e3f52405aa83
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2021-06-23 19:37:43 -0400

    Add files via upload

---
+++ string_letter_counter.py
@@ -0,0 +1,10 @@
+def count_letters_in_string(string):
+    letter_counts = {}
+    string = string.lower()
+    for letter in string:
+        if letter.isalpha():
+            if letter not in letter_counts:
+                letter_counts[letter] = 0 
+        letter_counts[letter] += 1
+    return letter_counts
+print(count_letters_in_string('hello'))
